076672c016ffb27dd86db07cfbde0a8a075893c3
CoderGwt <1341680843@qq.com>
2019-03-19 11:20:05 +0800

    hots finished and switch

--- pages/movie/index.js
+++ pages/movie/index.js
@@ -1,3 +1,19 @@
 Page({
+  data: {
+    tabIndex: 'hots',
+  },
   
+  switch: function(ev){
+    console.log(ev)
+    // 通过自定义属性 tabIndex 的值来判断用户的点击
+    var tabIndex = ev.target.dataset.tabIndex;
+    console.log(tabIndex);
+
+    // 通过setData设置 来实现数据的更改
+    // 
+    this.setData({
+      tabIndex: tabIndex
+    })
+  }
+
 })
